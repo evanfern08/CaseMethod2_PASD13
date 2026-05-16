6b1ac0589636ea9fab2799908df5c09e6799acdc
evan1234-map <evanfernanda1708@gmail.com>
2026-05-16 22:35:23 +0700

    Class ini menghubungkan data pembeli dengan node sebelumnya dan node berikutnya

---
+++ NodePembeli09.java
@@ -0,0 +1,11 @@
+public class NodePembeli09 {
+    Pembeli09 data;
+    NodePembeli09 next;
+    NodePembeli09 prev;
+
+    public NodePembeli09(Pembeli09 data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
