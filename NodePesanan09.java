40d769b913932aa0a14cc079033eb8696267c834
evan1234-map <evanfernanda1708@gmail.com>
2026-05-16 22:40:01 +0700

    Node ini menyimpan objek pesanan serta penghubung antar node

---
+++ NodePesanan09.java
@@ -0,0 +1,11 @@
+public class NodePesanan09 {
+    Pesanan09 data;
+    NodePesanan09 next;
+    NodePesanan09 prev;
+
+    public NodePesanan09(Pesanan09 data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
